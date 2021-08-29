31e53b10a3f1727eebe5411345849ff16509863b
Jitendra Manish <jmanishm7@gmail.com>
2021-08-29 11:54:38 +0530

    Created Account Microservice - Gets registered in Eureka Service

---
+++ PixlrPicAppApiAccountMangement/src/test/java/com/mauryaApp/pixlrPicApp/api/account/PixlrPicAppApiAccountMangementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mauryaApp.pixlrPicApp.api.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixlrPicAppApiAccountMangementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
